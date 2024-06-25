65eecf5b18a116ebc163488f903c9804b90ce1b2
ang3laaa7 <angelachanc321@gmail.com>
2024-06-25 10:47:00 -0400

    Initial Commit

---
+++ Questionapp/QuestionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionappApp.swift
+//  Questionapp
+//
+//  Created by Angela on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
